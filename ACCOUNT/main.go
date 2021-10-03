ee85d33ba49931b253997433435b0d15b8f48cb1
zaehuun <jdrjk0000@naver.com>
2021-10-03 13:25:33 +0900

    refactor : 디렉토리 이름을 변경한다.

---
+++ ACCOUNT/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/zaehuun/LetItGO/ACCOUNT/accounts"
+)
+
+func main() {
+	account := accounts.NewAccount("zaehuun")
+	account.Deposit(20)
+	err := account.Withdraw(20)
+	fmt.Println(account)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(account.Balance(), account.Owner())
+}
